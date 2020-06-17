2795c71b43d31efd8090a91e613b133aeef37880
Avijit Karmaker <ajkacca@gmail.com>
2020-06-17 11:53:04 +0300

    All files added

---
+++ lovelife.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+$(document).ready(function(){
+  $("a").on('click', function(e) {
+    if (this.hash !== "") {
+        e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
